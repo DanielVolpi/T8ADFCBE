8dfc3f090104dd11fe9d94f17107abc25191686e
Dann Oak <dann.inbox+github@proton.me>
2024-03-19 19:10:32 -0300

    chore: removing logs and unused mockdata

--- server.js
+++ server.js
@@ -11,16 +11,11 @@ mongoose.connect(process.env.DATABASE_URI);
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => {
-  console.log('Connected to the DataBase');
   app.listen(process.env.PORT);
 });
 
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
 
 const specialistRouter = require('./routes/specialists');
 const userRouter = require('./routes/users');
